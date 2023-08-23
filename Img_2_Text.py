2122e6b285e9c1655a9bf0e29b89594fa96bb4db
TwoChill <mdefance1988@gmail.com>
2023-08-23 18:21:08 +0200

    I've added a delete function that deletes images from the Telegram server using the Telegram Bot API. With a variable set to True or False to activate of deactivate the function.

--- Img_2_Text.py
+++ Img_2_Text.py
@@ -2,45 +2,47 @@ import os
 import requests
 import pytesseract
 import configparser
+from io import BytesIO
+from PIL import Image
 
+# Read configuration from the settings file
 config = configparser.ConfigParser()
 config.read("settings.config")
-
-# Add your own Telegram Bot Token and ChatID to your own .config file.
 log_bot_token = config['settings']['log_bot_token']
 log_bot_id = config['settings']['log_bot_id']
 
+# # Delete the received image message from the chat if True
+delete_img = False
 
-def extract_text_from_image(image_path):
-    extracted_text = pytesseract.image_to_string(image_path)
+
+# Extract text from image data using OCR
+def extract_text_from_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    extracted_text = pytesseract.image_to_string(image)
     return extracted_text
 
 
+# Send text message to a chat using the Telegram API
 def send_text_to_telegram(chat_id, text):
     send_text = f'https://api.telegram.org/bot{log_bot_token}/sendMessage?chat_id={chat_id}&text={text}'
     requests.get(send_text)
 
 
-def process_received_image(file_id, chat_id):
-    file_info = requests.get(f'https://api.telegram.org/bot{log_bot_token}/getFile?file_id={file_id}').json()
-    file_path = file_info['result']['file_path']
-
-    image_url = f'https://api.telegram.org/file/bot{log_bot_token}/{file_path}'
-    image_response = requests.get(image_url)
-
-    image_filename = os.path.basename(file_path)
-    with open(image_filename, 'wb') as img_file:
-        img_file.write(image_response.content)
-
-    extracted_text = extract_text_from_image(image_filename)
-
+# Process received image and send extracted text back
+def process_received_image(image_data, chat_id, message_id):
+    extracted_text = extract_text_from_image(image_data)
     send_text_to_telegram(chat_id, extracted_text)
-    os.remove(image_filename)
+
+    if delete_img is True:
+        # Delete the received image message from the chat
+        delete_message_url = f'https://api.telegram.org/bot{log_bot_token}/deleteMessage?chat_id={chat_id}&message_id={message_id}'
+        requests.get(delete_message_url)
 
 
+# Main function to listen for incoming messages and process images
 def main():
     print("Listening for incoming messages...")
-    bot_message = 'Ik ben klaar om je foto\'s te ontvangen!'
+    bot_message = "I'm ready to convert your photos!"
     send_text_to_telegram(log_bot_id, bot_message)
     offset = None
 
@@ -49,8 +51,17 @@ def main():
         for update in response['result']:
             if 'message' in update and 'photo' in update['message']:
                 chat_id = update['message']['chat']['id']
+                message_id = update['message']['message_id']  # Get the message ID
                 file_id = update['message']['photo'][-1]['file_id']
-                process_received_image(file_id, chat_id)
+
+                # Fetch the file data directly using getFile and download it
+                file_info = requests.get(
+                    f'https://api.telegram.org/bot{log_bot_token}/getFile?file_id={file_id}').json()
+                file_path = file_info['result']['file_path']
+                file_url = f'https://api.telegram.org/file/bot{log_bot_token}/{file_path}'
+                image_response = requests.get(file_url).content
+
+                process_received_image(image_response, chat_id, message_id)  # Pass the message ID
 
             offset = update['update_id'] + 1
 
