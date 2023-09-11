cd098ff93d2c315605046790abf43e26c895e40a
Michael <mdefrance1988@gmail.com>
2023-09-11 12:45:07 +0200

    refactor(telegram-bot): Update and improve the Telegram bot script
    - Improved code organization and readability.
    - Added docstrings and comments for better code documentation.
    - Modified the image processing logic to prevent repeated processing of the same updates.
    - Updated function and variable names for clarity.
    - Enhanced text extraction and formatting.
    
    These changes aim to make the script more robust and maintainable.

--- Img_2_Text.py
+++ Img_2_Text.py
@@ -1,9 +1,10 @@
 import os
 import requests
-import pytesseract
 import configparser
-from io import BytesIO
-from PIL import Image
+import easyocr
+import multiprocessing
+import sys
+import textwrap
 
 # Read configuration from the settings file
 config = configparser.ConfigParser()
@@ -14,23 +15,108 @@ log_bot_id = config['settings']['log_bot_id']
 # # Delete the received image message from the chat if True
 delete_img = False
 
+# Function to perform text recognition
+def perform_text_recognition(image_path, result_queue, image_count):
+    """
+    Generate an image from the provided text and save it to the specified file path.
+
+    Args:
+        text (str): The text content that you want to convert into an image.
+        image_path (str): The file path where the generated image will be saved.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If there is an error during image generation or saving.
+
+    Example:
+        generate_text_image("Hello, World!", "hello_image.png")
+    """
+
+    reader = easyocr.Reader(["en", "nl"], gpu=False, verbose=False)
+
+    with open(os.devnull, 'w') as null_output:
+        original_stdout = sys.stdout
+        sys.stdout = null_output
+        result = reader.readtext(image_path)
+        sys.stdout = original_stdout
+
+    result_queue.put(result)
+
+# Function to clear the screen based on the platform
+def clear_screen():
+    if sys.platform.startswith('win'):
+        os.system('cls')
+    else:
+        os.system('clear')
 
 # Extract text from image data using OCR
 def extract_text_from_image(image_data):
-    image = Image.open(BytesIO(image_data))
-    extracted_text = pytesseract.image_to_string(image)
-    return extracted_text
+    # Create a temporary file to save the image
+    with open("temp_image.jpg", "wb") as img_file:
+        img_file.write(image_data)
 
+    # Perform text recognition on the saved image
+    result_queue = multiprocessing.Queue()
+    process = multiprocessing.Process(target=perform_text_recognition, args=("temp_image.jpg", result_queue, 1))
+    process.start()
+    process.join()
+    result = result_queue.get()
+
+    # Extract recognized text and bounding boxes
+    recognized_text = [detection[1] for detection in result]
+    bounding_boxes = [detection[0] for detection in result]
+
+    # Extracting newlines and paragraphs based on bounding box positions
+    lines = []
+    current_line = ""
+
+    for box, text in zip(bounding_boxes, recognized_text):
+        x, y, w, h = box
+        if len(lines) > 0 and len(current_line) > 0 and y > lines[-1][1] + lines[-1][3] * 0.5:
+            lines.append((current_line.strip(), x, y, w, h))
+            current_line = text
+        else:
+            current_line += " " + text
+
+    # Adding the last line
+    lines.append((current_line.strip(), x, y, w, h))
+
+    # Sorting the lines based on y-coordinate
+    lines.sort(key=lambda x: x[2])
+
+    # Reconstructing paragraphs based on vertical position
+    paragraphs = []
+    current_paragraph = ""
+
+    for line, x, y, w, h in lines:
+        if len(paragraphs) > 0 and len(current_paragraph) > 0 and x < paragraphs[-1][3] * 0.5:
+            paragraphs.append(current_paragraph.strip())
+            current_paragraph = line
+        else:
+            current_paragraph += " " + line
+
+    # Adding the last paragraph
+    paragraphs.append(current_paragraph.strip())
+
+    # Create a formatted text with text wrapping
+    formatted_text = ""
+    for paragraph in paragraphs:
+        wrapped_paragraph = textwrap.fill(paragraph, width=80, break_long_words=False)
+        formatted_text += f"Recognized Text:\n{wrapped_paragraph}\n"
+
+    return formatted_text
 
 # Send text message to a chat using the Telegram API
 def send_text_to_telegram(chat_id, text):
     send_text = f'https://api.telegram.org/bot{log_bot_token}/sendMessage?chat_id={chat_id}&text={text}'
     requests.get(send_text)
 
-
 # Process received image and send extracted text back
 def process_received_image(image_data, chat_id, message_id):
     extracted_text = extract_text_from_image(image_data)
+
     send_text_to_telegram(chat_id, extracted_text)
 
     if delete_img is True:
@@ -38,14 +124,15 @@ def process_received_image(image_data, chat_id, message_id):
         delete_message_url = f'https://api.telegram.org/bot{log_bot_token}/deleteMessage?chat_id={chat_id}&message_id={message_id}'
         requests.get(delete_message_url)
 
-
 # Main function to listen for incoming messages and process images
 def main():
     print("Listening for incoming messages...")
-    bot_message = "I'm ready to convert your photos!"
-    send_text_to_telegram(log_bot_id, bot_message)
+    send_text_to_telegram(log_bot_id, "I'm ready to convert your photos!")
+
+    # Get the last message ID from the chat
     offset = None
 
+    # Keep listening for incoming messages
     while True:
         response = requests.get(f'https://api.telegram.org/bot{log_bot_token}/getUpdates?offset={offset}').json()
         for update in response['result']:
@@ -61,10 +148,16 @@ def main():
                 file_url = f'https://api.telegram.org/file/bot{log_bot_token}/{file_path}'
                 image_response = requests.get(file_url).content
 
+                # Process the image and send the extracted text
+                # This prevents your bot from repeatedly processing the same updates.
                 process_received_image(image_response, chat_id, message_id)  # Pass the message ID
 
+            # Update the offset for the next request.
             offset = update['update_id'] + 1
 
+            # # Exit the lambda function.
+            # print("Done!")
+            # return
 
 if __name__ == "__main__":
     main()
